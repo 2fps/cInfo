b16eeb8586792427191e2578c4804c43ed793051
2fps <echoweb@126.com>
2018-06-10 23:04:01 +0800

    add module file

---
+++ app.js
@@ -0,0 +1,9 @@
+/*jshint esversion: 6 */
+/*const http = require('http');
+const fs = require('fs');
+const request = require('request');*/
+// const iconv = require('iconv-lite');
+const mongoose = require("./dataDB/db.js");
+const film = require('./film/film.js');
+
+film.filmStart();
